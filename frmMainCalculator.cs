4bde21b7423b056f43782c818e0befde322d90d0
HuyenMy <huyenmy160505@gmail.com>
2025-11-06 19:15:08 +0700

    Giao dien form co ban

---
+++ frmMainCalculator.cs
@@ -0,0 +1,10 @@
+namespace PhanMemMayTinh
+{
+    public partial class frmMainCalculator : Form
+    {
+        public frmMainCalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
